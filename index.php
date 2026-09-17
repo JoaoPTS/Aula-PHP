3a8dc45ff68b1af31d430f91b7f7ae2e505513d6
JoaoPTS <joao.souza123121@gmail.com>
2026-09-16 21:56:12 -0300

    Exercicio 3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Pagina Inicia</title>
+	<title>Página Inicial</title>
 	<link rel="stylesheet" href="style.css">
 </head>
 
