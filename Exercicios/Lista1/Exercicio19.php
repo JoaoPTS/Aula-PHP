f4eec87daae07c624fa424500532355adf13e850
Marco Kameoka <marcokameoka@gmail.com>
2026-09-18 15:46:52 -0300

    Exercicio 19 e final da lista 1

---
+++ Exercicios/Lista1/Exercicio19.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exercício 19</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+		crossorigin="anonymous"></script>
+	<nav class="navbar navbar-light bg-light">
+		<a class="btn btn-primary ms-3" href="/index.php">Voltar para a Página Principal</a>
+	</nav>
+	<div class="container mt-5">
+		<h1 class="text-left" style="font-size: 20px;">Cálculo da velocidade média:</h1>
+		<form action="" method="post" class="mt-4">
+			<div class="d-flex flex-column align-items-start gap-2 mb-3">
+				<div class="d-flex align-items-center gap-2">
+					<label for="distancia">Distância percorrida (m):</label>
+					<input type="number" min="1" id="distancia" name="distancia" class="form-control" style="width: 100px;" required>
+				</div>
+				<div class="d-flex align-items-center gap-2">
+					<label for="tempo">Tempo (s):</label>
+					<input type="number" min="1" id="tempo" name="tempo" class="form-control" style="width: 100px;" required>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-success" style="font-size: 20px;">Calcular</button>
+		</form>
+	</div>
+	<?php
+		$distancia = $_POST['distancia'] ?? 0;
+		$tempo = $_POST['tempo'] ?? 1;
+		$velocidadeMedia = $distancia / $tempo;
+	?>
+	<div class="container mt-3">
+		<h2 class="text-left" style="font-size: 20px;">Resultado: <?php echo 'Velocidade média = ' . $velocidadeMedia; ?></h2>
+	</div>
+</body>
+
+</html>
