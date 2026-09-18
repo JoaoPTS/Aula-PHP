5cf8bed5133fa2faf382866e0712a5d55e284c44
Marco Kameoka <marcokameoka@gmail.com>
2026-09-18 15:01:27 -0300

    Exercicio 6

---
+++ Exercicios/Lista1/Exercicio6.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exercício 6</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+		crossorigin="anonymous"></script>
+	<nav class="navbar navbar-light bg-light">
+		<a class="btn btn-primary ms-3" href="/index.php">Voltar para a Página Principal</a>
+	</nav>
+	<div class="container mt-5">
+		<h1 class="text-left" style="font-size: 20px;">Conversão de Temperatura F° para C°:</h1>
+		<form action="" method="post" class="mt-4">
+			<div class="d-flex flex-column align-items-start gap-2 mb-3">
+				<div class="d-flex align-items-center gap-2">
+					<label for="temp">Temperatura F°:</label>
+					<input type="number" step="0.01" id="temp" name="temp" class="form-control" style="width: 100px;" required>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-success" style="font-size: 20px;">Calcular</button>
+		</form>
+	</div>
+	<?php
+	$temp = $_POST['temp'] ?? 0;
+
+	$temp_f = ($temp - 32) * 5/9;
+	?>
+	<div class="container mt-3">
+		<h2 class="text-left" style="font-size: 20px;">Resultado: <?php echo 'Temperatura ' . number_format($temp, 2, ',', '.') . 'F° = ' . number_format($temp_f, 2, ',', '.') . 'C°'; ?></h2>
+	</div>
+</body>
+
+</html>
