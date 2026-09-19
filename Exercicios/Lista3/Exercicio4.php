f204cf6b0301be056eb14e6d0c2104f2cefc0352
JoaoPTS <joao.souza123121@gmail.com>
2026-09-19 02:21:54 -0300

    Exercicio 4

---
+++ Exercicios/Lista3/Exercicio4.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exercício 3</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+		crossorigin="anonymous"></script>
+	<nav class="navbar navbar-light bg-light">
+		<a class="btn btn-primary ms-3" href="/index.php">Voltar para a Página Principal</a>
+	</nav>
+	<div class="container mt-5">
+		<h1 class="text-left" style="font-size: 20px;">Verificar se a data é valida:</h1>
+		<form action="" method="post" class="mt-4">
+			<div class="d-flex flex-column align-items-start gap-2 mb-3">
+				<div class="d-flex align-items-center gap-2">
+					<label for="dia">Dia:</label>
+					<input type="number" id="dia" name="dia" class="form-control me-2" style="width: 200px;" required>
+				</div>
+			</div>
+			<div class="d-flex flex-column align-items-start gap-2 mb-3">
+				<div class="d-flex align-items-center gap-2">
+					<label for="mes">Mês:</label>
+					<input type="number" id="mes" name="mes" class="form-control me-2" style="width: 200px;" required>
+				</div>
+			</div>
+			<div class="d-flex flex-column align-items-start gap-2 mb-3">
+				<div class="d-flex align-items-center gap-2">
+					<label for="ano">Ano:</label>
+					<input type="number" id="ano" name="ano" class="form-control me-2" style="width: 200px;" required>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-success" style="font-size: 20px;">Calcular</button>
+		</form>
+	</div>
+	<?php
+	$dia = $_POST['dia'] ?? 0;
+	$mes = $_POST['mes'] ?? 0;
+	$ano = $_POST['ano'] ?? 0;
+	$dataValida = checkdate($mes, $dia, $ano);
+  if ($dia > 0 || $mes > 0 || $ano > 0) {
+	echo "<div class='container mt-3'>
+          <h2 class='text-left' style='font-size: 20px;'>Resultado: A data " . ($dataValida ? "é válida" : "não é válida") . ".</h2>
+        </div>";           
+  }
+	?>
+</body>
+
+</html>
